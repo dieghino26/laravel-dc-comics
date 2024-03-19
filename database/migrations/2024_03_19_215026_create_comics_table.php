0fe54fd1857756993ead428427a8bfb707d30539
dieghino26 <brotzadiego@gmail.com>
2024-03-19 22:54:37 +0100

    added migrations info

--- database/migrations/2024_03_19_215026_create_comics_table.php
+++ database/migrations/2024_03_19_215026_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('description')->nullable();
+            $table->text('thumb');
+            $table->string('price', 10);
+            $table->string('series', 80);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
